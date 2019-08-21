b693e5eb0cd6fe45a7a77076077adf9f86e96515
Artur Kida <ackida@gmail.com>
2019-08-21 00:46:44 -0300

    first call to API from Activity

--- app/src/main/java/com/arturkida/eurorates/MainActivity.kt
+++ app/src/main/java/com/arturkida/eurorates/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.arturkida.eurorates
 
+import android.arch.lifecycle.Observer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.arturkida.eurorates.model.Currency
+import com.arturkida.eurorates.repository.ApiImpl
 
 class MainActivity : AppCompatActivity() {
+
+    private val apiImpl = ApiImpl()
+    private lateinit var currency: Currency
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getCurrencies()
+    }
+
+    fun getCurrencies() {
+        apiImpl.getCurrencies().observe(this, Observer {currencies ->
+            currencies?.let {
+                currency = it
+                Log.i("currency", currency.toString())
+            }
+        })
     }
 }
