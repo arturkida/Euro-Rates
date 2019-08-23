d75e2b2d869953bf5fcb1a0084920049c620f563
Artur Kida <ackida@gmail.com>
2019-08-23 00:29:38 -0300

    creating home screen layout

--- app/src/main/java/com/arturkida/eurorates/ui/CurrencyActivity.kt
+++ app/src/main/java/com/arturkida/eurorates/ui/CurrencyActivity.kt
@@ -3,10 +3,8 @@ package com.arturkida.eurorates.ui
 import android.arch.lifecycle.Observer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import com.arturkida.eurorates.R
-import com.arturkida.eurorates.model.Currency
 import com.arturkida.eurorates.viewmodel.CurrencyViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -16,7 +14,7 @@ class CurrencyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_currency)
 
         getCurrencies()
     }
